3b88593aa8d4c9d459460381744ad6638f6af3a3
SimonMaracine <simonmaracine@gmail.com>
2025-06-14 14:16:20 +0300

    Copy quiz link to clipboard

---
+++ bs-free-friendship-test/static/copy_to_clipboard.js
@@ -0,0 +1,7 @@
+function copy_link_to_clipboard() {
+    const quiz_link = document.getElementById("quiz_link")
+
+    navigator.clipboard.writeText(quiz_link.value)
+
+    alert("Link copied!");
+}
